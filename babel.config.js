f71d9ef5b9ff368e4b63189bbf8e42415aa4248b
mynameisbei <1850055742@qq.com>
2021-06-29 17:49:23 +0800

    feat(vue-element-admin): remove jest

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,15 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: [
-    process.env.IS_JEST !== 'Y'
-      ? [
-          'import',
-          {
-            libraryName: 'element-plus',
-            customStyleName: (name) => {
-              name = name.slice(3);
-              return `element-plus/packages/theme-chalk/src/${name}.scss`;
-            },
-          },
-        ]
-      : {},
+    [
+      'import',
+      {
+        libraryName: 'element-plus',
+        customStyleName: (name) => {
+          name = name.slice(3);
+          return `element-plus/packages/theme-chalk/src/${name}.scss`;
+        },
+      },
+    ],
   ],
 };
